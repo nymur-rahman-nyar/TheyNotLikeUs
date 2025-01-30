55349ac16ad82aa30e5dde22ebec39f56bdaf7a8
KarlFranzman <kehan@ualberta.ca>
2025-01-29 20:27:44 -0700

    add color

--- app/src/main/java/com/example/theynotlikeus/Triangle.java
+++ app/src/main/java/com/example/theynotlikeus/Triangle.java
@@ -1,9 +1,13 @@
 package com.example.theynotlikeus;
 
-public class Triangle extends Shape{
+
+import android.graphics.drawable.shapes.Shape;
+
+public abstract class Triangle extends Shape {
     int x = 10;
     int y = 10;
     int z = 10;
+    String color = "blue";
     public int sum(int a,int b,int c){
         return a+b+c;
     };
