b86df0c2af0bf16801b85b839c6ea056c06efad5
subaita <subaita1@ualberta.ca>
2025-01-29 21:23:50 -0700

    final submission

---
+++ app/src/main/java/com/example/theynotlikeus/Circle.java
@@ -0,0 +1,8 @@
+package com.example.theynotlikeus;
+
+public class Circle extends Shape{
+  int x = 1;
+  int y = 1;
+  public Circle(int x) {
+    int r = x;
+}
---
+++ app/src/main/java/com/example/theynotlikeus/Triangle.java
@@ -0,0 +1,10 @@
+package com.example.theynotlikeus;
+
+public class Triangle extends Shape{
+    int x = 10;
+    int y = 10;
+    int z = 10;
+    public int sum(int a,int b,int c){
+        return a+b+c;
+    };
+}
